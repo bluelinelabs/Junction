5ded5e4edd5482dfc56d3fecb2b92d2b92cbd908
Jimmy McDermott <jimmy@bluelinelabs.com>
2016-06-24 10:31:52 -0500

    add wave window file

---
+++ Wave/Wave/WaveWindow.swift
@@ -0,0 +1,18 @@
+//
+//  WaveWindow.swift
+//  Wave
+//
+//  Created by Jimmy McDermott on 6/24/16.
+//  Copyright © 2016 Blue Line Labs. All rights reserved.
+//
+
+import UIKit
+
+//Subclassing UIWindow allows the user to set the window in the App Delegate, and then not worry about it anywhere else in the app. In all essence, this allows the user to access the pullout on any screen in the app
+public class WaveWindow: UIWindow {
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
