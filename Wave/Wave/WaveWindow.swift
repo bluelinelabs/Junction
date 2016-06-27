a2e7c381bbf0ff8224ac1ce1fa063adde7703c4d
Jimmy McDermott <jimmy@bluelinelabs.com>
2016-06-27 10:16:21 -0500

    Update Wave Window

--- Wave/Wave/WaveWindow.swift
+++ Wave/Wave/WaveWindow.swift
@@ -11,16 +11,12 @@ import UIKit
 //Subclassing UIWindow allows the user to set the window in the App Delegate, and then not worry about it anywhere else in the app. In all essence, this allows the user to access the pullout on any screen in the app
 public class WaveWindow: UIWindow {
     
-    private var keeper: WaveKeeper
-    private var side: Side
-    private var isDebug = false
-    private var waveViewController: WaveViewController
+    private var style: PresentationStyle
+    private var enabled: Bool
     
-    public init(frame: CGRect, keeper: WaveKeeper, side: Side) {
-        self.keeper = keeper
-        self.side = side
-        
-        self.waveViewController = WaveViewController(waveKeeper: keeper)
+    public init(frame: CGRect, style: PresentationStyle, enabled: Bool) {
+        self.style = style
+        self.enabled = enabled
         
         super.init(frame: frame)
     }
@@ -30,7 +26,8 @@ public class WaveWindow: UIWindow {
     }
 }
 
-public enum Side {
-    case Left
-    case Right
+public enum PresentationStyle {
+    case LeftDrawer
+    case RightDrawer
+    case Shake
 }
